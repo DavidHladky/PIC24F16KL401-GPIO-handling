b5fc52ef9903f1cf55452974d9b636efc7baab96
David Hladký <hladkyd@gmail.com>
2017-07-17 20:56:27 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -10,30 +10,30 @@
 
 int main(void) {
     /*  TRISx - This register sets the direction of the port
-     *  0 - A pin is set as input
-     *  1 - A pin is set as outpt 
+     *  0 - GPIO pin is set as output
+     *  1 - GPIO pin is set as input
      * 
-     *  PORTx - This register reads a content of the port/pin
-     *  LATx - This register writes a data to the port/pin 
+     *  PORTx - This register reads a content of a GPIO
+     *  LATx - This register writes data to the specified GPIO 
      * 
-     *  ANSx This register controls an analog input of desired ports
+     *  ANSx This register controls an analog input of desired GPIO
      *  0 - Digital input buffer is active - must be cleared if you want to use peripherals SPI, I2C, USART and etc...
      *  1 - Digital input buffer is not active (use for analog input)
      */
-    TRISAbits.TRISA4 = 1;  // Pin RA4 is set as the output (LED Diode)
-    TRISBbits.TRISB4 = 0; // Pin RB4 is set as the input (Button)
+    TRISAbits.TRISA4 = 1;  // Pin RA4 is set as the input (Button)
+    TRISBbits.TRISB4 = 0; // Pin RB4 is set as the output (LED diode)
     ANSBbits.ANSB15 = 0; // Analog input on RB4 off
-    LATBbits.LATB4 = 1;  // Set RB4 to logical 1
+    LATBbits.LATB4 = 1;  // Set RB4 to logical 1 - turn ON LED
     
     while(1) // Endless loop
     {
-        if(!PORTAbits.RA4) // If is the button pressed RA = 0
+        if(!PORTAbits.RA4) // If is the button pressed, GPIO RA4 = 0
         {
-            LATBbits.LATB4 = 0; // Set RB4 to 0
+            LATBbits.LATB4 = 0; // Set RB4 to 0 - Turn OFF LED diode
         }
-        else // If is not the button pressed RA = 1
+        else // If the button is not pressed, GPIO RA4 = 1
         {
-           LATBbits.LATB4 = 1;  // Set RB4 to 1
+           LATBbits.LATB4 = 1;  // Set RB4 to 1 - Turn IN LED diode
         }       
     }
     return 0;
